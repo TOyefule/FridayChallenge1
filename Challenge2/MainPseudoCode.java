ba3a2150208c5fc268f30d6991e4c7dcc24d55bc
Tolani Oyefule <TOyefule@users.noreply.github.com>
2019-02-01 15:01:52 -0500

    Create MainPseudoCode.java

---
+++ Challenge2/MainPseudoCode.java
@@ -0,0 +1,58 @@
+/Challenge 2 Psuedo Code
+
+
+Initialize Program Main() {
+var A =  variable A;
+var B = variable B;
+var high = variable high;
+var low = variable low;
+var red = variable red;
+var black = variable black;
+var money = 0;
+var betAmount = 0;
+var gameswon = 0;
+var gameslost = 0;
+
+
+System.out.println( " WELCOME! Let's play some Roulette!");
+
+System.out.println("Enter the type of bet you would like to place (red|black|even|odd|high|low): ")
+System.in( red)
+	if(Argument)
+Scanner sc = new Scanner
+
+System.out.println("Enter in your bet amount: ")
+	System.in(35)
+System.out.println(" The ball landed on " random.int(27) + random.color(black) + ".")
+if(state.col == true && state.num == true) {
+	System.out.println("Congratulations, you've won.")
+} else {System.out.println("Sorry, you've lost this bet.")
+
+system.out.println("You currently have: $" money(-35.0) )
+
+// You currently have: $-35.0
+
+system.out.println("Would you like to play again (true|false)?")
+	System.in(true)
+/*	
+Enter the type of bet you would like to place (red|black|even|odd|high|low): low
+Enter in your bet amount: 40
+The ball landed on 17 black.
+Congratulations, you've won.
+
+You currently have: $45.0
+
+Would you like to play again (true|false)? true
+Enter the type of bet you would like to place (red|black|even|odd|high|low): even
+Enter in your bet amount: 100
+The ball landed on 29 black.
+Sorry, you've lost this bet.
+
+You currently have: $-55.0
+Would you like to play again (true|false)? false 
+
+*/
+System.out.println("Thank you for playing!")
+System.out.println("This is how much money you have remaining: $" + money(-55.00))
+System.out.println("You've won " + gameswon +" game(s) and lost " +gameslost+ " game(s).")
+}
